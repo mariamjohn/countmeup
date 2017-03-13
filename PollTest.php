397d55a6bf232ed2728452cc029446023e9b85fe
mariamjohn <mariam.john@intofilm.org>
2017-03-13 00:23:37 +0000

    Update PollTest.php

--- PollTest.php
+++ PollTest.php
@@ -21,9 +21,3 @@ class PollTest extends FC_Test_PHPUnit_Unit_Zend_Db_Table
     }
 }
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
